0d694abf6db4ef71c9919db1b24d5b61843e6012
Eclipsed13git <dibyangshu2006@gmail.com>
2026-02-05 23:58:24 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,6 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
---
+++ program2.py
@@ -0,0 +1,3 @@
+x=input("enter your first name:")
+y=input("enter your last name:")
+print("Hello, "+ x+" "+y+"! ""Welcome to the Python program.")
